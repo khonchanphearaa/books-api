250359263e072b1dad8f2a710f44d685f211e048
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-07 15:11:21 +0700

    feat: add isAdmin middleware to book routes for role-based access control

--- book-api/src/routes/bookRoute.ts
+++ book-api/src/routes/bookRoute.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import * as bookController from '../controllers/bookController.js'
+import { isAdmin } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.get('/', bookController.getAllBooks);
 router.get('/:id', bookController.getBookById);
-router.post('/', bookController.addBook);
-router.put('/:id', bookController.updateBook);
-router.delete('/:id', bookController.deleteBook);
+router.post('/', isAdmin, bookController.addBook);
+router.put('/:id', isAdmin, bookController.updateBook);
+router.delete('/:id', isAdmin, bookController.deleteBook);
 
 export default router;
