703c86e6596cd9365ce6759e4452e3cd75844c50
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-12 21:25:09 +0700

    feat: add getCategoryById, updateCategory, and deleteCategory functions to category controller

--- book-api/src/controllers/categoryController.ts
+++ book-api/src/controllers/categoryController.ts
@@ -35,3 +35,77 @@ export const getAllCategories = async (req: Request, res: Response) =>{
         allCategories
     })
 }
+
+export const getCategoryById = async (req: Request, res: Response) =>{
+    const {id} = req.params;
+    try {
+        const category = await prisma.category.findUnique({
+            where: {id: parseInt(id as string, 10)},
+        })
+        if(!category){
+            return res.status(404).json({
+                message: 'Category not found'
+            })
+        }
+        res.status(200).json({
+            message: 'Get category by id success',
+            category
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error fetching category'
+        })
+    }
+}
+
+export const updateCategory = async (req: Request, res: Response) =>{
+    const {id}=req.params;
+    const {name}=req.body;
+    try {
+        const categoryExisting = await prisma.category.findUnique({
+            where: {id: parseInt(id as string, 10)},
+        })
+        console.log(categoryExisting);
+        if(!categoryExisting){
+            return res.status(404).json({
+                message: 'Category not found'
+            })
+        }
+        const updateCategory = await prisma.category.update({
+            where: {id: parseInt(id as string, 10)},
+            data: {name}
+        })
+        res.status(200).json({
+            message: 'Category updated successfully',
+            updateCategory
+        })
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error updating category'
+        })
+    }
+    
+}
+
+
+export const deleteCategory = async (req: Request, res: Response) =>{
+    const {id} = req.params;
+    try {
+        const existing = await prisma.category.findUnique({
+            where: { id: parseInt(id as string, 10) }
+        });
+
+        if (!existing) {
+            return res.status(404).json({ message: 'Category not found' });
+        }
+
+        await prisma.category.delete({
+            where: { id: parseInt(id as string, 10) }
+        });
+
+        return res.status(200).json({ message: 'Delete category success' });
+    } catch (error) {
+        return res.status(500).json({ error: 'Failed to delete category' });
+    }
+}
