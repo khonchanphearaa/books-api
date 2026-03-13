50d6e39ebf5a8eb5cdd5076b7660983091b8fcd1
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-13 21:55:59 +0700

    feat: add Prisma client setup with PostgreSQL adapter

---
+++ book-api/src/utils/prisma.ts
@@ -0,0 +1,11 @@
+import { Pool } from 'pg';
+import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
