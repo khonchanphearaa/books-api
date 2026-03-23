424faca3eed0167ff3bdd92ef0c0c55c7fb8fc0f
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-23 16:42:12 +0700

    feat: enhance categoryController with duplicate error handling and improved response messages

--- book-api/src/controllers/categoryController.ts
+++ book-api/src/controllers/categoryController.ts
@@ -1,8 +1,12 @@
 import type { Request, Response } from 'express';
 import { sendResponse } from '../utils/response.js';
+import { Prisma } from '@prisma/client';
 import prisma from '../utils/prisma.js';
 
-/* Create a new category */
+const isDuplicateError = (error: unknown): error is Prisma.PrismaClientKnownRequestError => {
+    return error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002';
+};
+
 export const createCategory = async (req: Request, res: Response) => {
     const { name } = req.body;
     try {
@@ -10,6 +14,9 @@ export const createCategory = async (req: Request, res: Response) => {
             data: { name }
         });
 
+        if (isDuplicateError(newCategory)) {
+            return res.status(400).json({ message: 'Category name already exists' });
+        }
         return sendResponse(res, 201, 'Create a category success', newCategory);
     } catch (error) {
         return sendResponse(res, 500, 'Error creating category');
@@ -33,7 +40,7 @@ export const getCategoryById = async (req: Request, res: Response) => {
             where: { id: parseInt(id as string, 10) },
         })
         if (!category) {
-            return sendResponse(res, 404, 'Category not found')
+            return res.status(404).json({ message: 'Category not found' });
         }
 
         return sendResponse(res, 200, 'Get category by id success', category);
@@ -51,15 +58,19 @@ export const updateCategory = async (req: Request, res: Response) => {
         })
         console.log(categoryExisting);
         if (!categoryExisting) {
-            return sendResponse(res, 404, 'Category not found');
+            return res.status(404).json({ message: 'Category not found' });
         }
         const updateCategory = await prisma.category.update({
             where: { id: parseInt(id as string, 10) },
             data: { name }
         })
 
+
         return sendResponse(res, 200, 'Category updated successfully', updateCategory);
     } catch (error) {
+        if (isDuplicateError(error)) {
+            return res.status(400).json({ message: 'Category name already exists', error: error.message });
+        }
         return sendResponse(res, 500, 'Error updating category');
     }
 
@@ -74,7 +85,7 @@ export const deleteCategory = async (req: Request, res: Response) => {
         });
 
         if (!existing) {
-            return sendResponse(res, 404, 'Category not found');
+            return res.status(404).json({ message: 'Category not found' });
         }
 
         await prisma.category.delete({
