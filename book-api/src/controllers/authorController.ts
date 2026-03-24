117467876da427dbe53b249f56bead7d84170677
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-24 22:06:10 +0700

    feat: implement update author functionality with validation and error handling

--- book-api/src/controllers/authorController.ts
+++ book-api/src/controllers/authorController.ts
@@ -54,7 +54,33 @@ export const authorController = {
         }
     },
 
-    /* Comming handler implementation Update, Delete of author */
+    async updateAuthor(req: Request, res: Response) {
+        try {
+            const { id } = req.params;
+            const { name, email, gender } = req.body;
+
+            const existingAuthor = await prisma.author.findUnique({
+                where: { id: parseInt(id as string, 10) }
+            });
+
+            if (!existingAuthor) {
+                return res.status(404).json({ message: 'Author not found!' });
+            }
+            const updatedAutor = await prisma.author.update({
+                where: { id: parseInt(id as string, 10) },
+                data: { name, email, gender: gender as Gender }
+            });
+            return sendResponse(res, 200, 'Update author success', updatedAutor);
+        } catch (error) {
+            let invalidGender = error instanceof Prisma.PrismaClientValidationError;
+            if (invalidGender) {
+                return res.status(400).json({ message: 'Invalid input. gender must be M, F, or UNKNOWN' })
+            }
+            return sendResponse(res, 400, 'Failed to update author');
+        }
+    },
+
+    /* Delete author nexting handler implementation.. */
 }
 
 
