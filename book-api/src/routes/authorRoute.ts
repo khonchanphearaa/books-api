7a046103bbf1cc29bd6e45ef7d54ffb4fbd1e56c
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-24 22:06:21 +0700

    feat: add update author route to author API

--- book-api/src/routes/authorRoute.ts
+++ book-api/src/routes/authorRoute.ts
@@ -6,5 +6,6 @@ const router = Router();
 router.post('/', authorController.CreateAuthor);
 router.get('/', authorController.getAllAuthors);
 router.get('/:id', authorController.getAuthorById);
+router.put('/:id', authorController.updateAuthor);
 
 export default router;
