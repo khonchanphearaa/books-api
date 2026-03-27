95fb45bcbc67d3b55a385a199d092405ee2158ba
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-27 23:56:09 +0700

    feat: refactor authorController to use service methods for data retrieval and updates

--- book-api/src/controllers/authorController.ts
+++ book-api/src/controllers/authorController.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from 'express';
 import prisma from '../utils/prisma.js';
 import { Gender, Prisma } from '@prisma/client';
 import { sendResponse } from '../utils/response.js';
+import {getAll, getById, update} from '../services/authorService.js';
 
 export const authorController = {
     async CreateAuthor(req: Request, res: Response) {
@@ -27,11 +28,7 @@ export const authorController = {
 
     async getAllAuthors(req: Request, res: Response) {
         try {
-            const allAuthors = await prisma.author.findMany({
-                include: {
-                    _count: { select: { books: true } }
-                }
-            })
+            const allAuthors = await getAll();
             return sendResponse(res, 200, 'Get all authors success', allAuthors);
         } catch (error) {
             return sendResponse(res, 400, 'Failed to get all authors');
@@ -41,16 +38,10 @@ export const authorController = {
     async getAuthorById(req: Request, res: Response) {
         try {
             const { id } = req.params;
-            const author = await prisma.author.findUnique({
-                where: { id: parseInt(id as string, 10) },
-            });
-
-            if (!author) {
-                return res.status(404).json({ message: 'Author not found' });
-            }
+            const author = await getById(id as string);
             return sendResponse(res, 200, 'Get author by id success', author);
-        } catch (error) {
-            return sendResponse(res, 400, 'Failed to get author by id');
+        } catch (error: any) {
+            return sendResponse(res, 400, error.message);
         }
     },
 
@@ -58,25 +49,10 @@ export const authorController = {
         try {
             const { id } = req.params;
             const { name, email, gender } = req.body;
-
-            const existingAuthor = await prisma.author.findUnique({
-                where: { id: parseInt(id as string, 10) }
-            });
-
-            if (!existingAuthor) {
-                return res.status(404).json({ message: 'Author not found!' });
-            }
-            const updatedAutor = await prisma.author.update({
-                where: { id: parseInt(id as string, 10) },
-                data: { name, email, gender: gender as Gender }
-            });
-            return sendResponse(res, 200, 'Update author success', updatedAutor);
-        } catch (error) {
-            let invalidGender = error instanceof Prisma.PrismaClientValidationError;
-            if (invalidGender) {
-                return res.status(400).json({ message: 'Invalid input. gender must be M, F, or UNKNOWN' })
-            }
-            return sendResponse(res, 400, 'Failed to update author');
+            const updatedAuthor = await update(id as string, { name, email, gender: gender as Gender });
+            return sendResponse(res, 200, 'Update author success', updatedAuthor);
+        } catch (error: any) {
+            return sendResponse(res, 400, error.message);
         }
     },
 
