c9b1e60ab30c498294e6ff5a4559b42c0d6b48bd
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-04-02 23:33:52 +0700

    feat: refactor authorController to use authorService for database operations and improve error handling

--- book-api/src/controllers/authorController.ts
+++ book-api/src/controllers/authorController.ts
@@ -1,28 +1,16 @@
 import type { Request, Response } from 'express';
-import prisma from '../utils/prisma.js';
-import { Gender, Prisma } from '@prisma/client';
+import { Gender } from '@prisma/client';
 import { sendResponse } from '../utils/response.js';
-import {getAll, getById, update} from '../services/authorService.js';
+import { getAll, getById, remove, update, create } from '../services/authorService.js';
 
 export const authorController = {
     async CreateAuthor(req: Request, res: Response) {
         try {
             const { name, email, gender } = req.body;
-            const newAutor = await prisma.author.create({
-                data: { name, email, gender: gender as Gender }
-            })
+            const newAutor = await create({ name, email, gender: gender as Gender });
             return sendResponse(res, 201, 'Create author success', newAutor);
-        } catch (error) {
-            let emailExist = error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002';
-            let invalidGender = error instanceof Prisma.PrismaClientValidationError;
-
-            if (emailExist) {
-                return res.status(400).json({ message: 'Email already exists' });
-            } else if (invalidGender) {
-                return res.status(400).json({ message: 'Invalid input. gender must be M, F, or UNKNOWN' })
-            } else {
-                return sendResponse(res, 400, 'Failed to create author');
-            }
+        } catch (error: any) {
+            return sendResponse(res, 400, error.message);
         }
     },
 
@@ -30,8 +18,8 @@ export const authorController = {
         try {
             const allAuthors = await getAll();
             return sendResponse(res, 200, 'Get all authors success', allAuthors);
-        } catch (error) {
-            return sendResponse(res, 400, 'Failed to get all authors');
+        } catch (error: any) {
+            return sendResponse(res, 400, error.message);
         }
     },
 
@@ -59,18 +47,10 @@ export const authorController = {
     async deleteAuthor(req: Request, res: Response) {
         const { id } = req.params;
         try {
-            const existingAuthor = await prisma.author.findUnique({
-                where: { id: parseInt(id as string, 10) }
-            })
-            if (!existingAuthor) {
-                return res.status(404).json({ message: 'Author not found!' });
-            }
-            await prisma.author.delete({
-                where: { id: parseInt(id as string, 10) }
-            })
+            await remove(id as string);
             return sendResponse(res, 200, 'Delete author success');
-        } catch (error) {
-            return sendResponse(res, 400, 'Failed to delete author');
+        } catch (error: any) {
+            return sendResponse(res, 400, error.message);
         }
     }
 }
